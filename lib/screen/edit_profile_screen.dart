95219e0ac3986f781bee98a0a496ed01db53e06c
jiyoung <ji9310@naver.com>
2019-10-25 10:50:49 +0900

    upload profile img & loading progress

--- lib/screen/edit_profile_screen.dart
+++ lib/screen/edit_profile_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:instagram_clone_app/models/user_model.dart';
 import 'package:instagram_clone_app/services/database_service.dart';
+import 'package:instagram_clone_app/services/storage_service.dart';
 
 class EditProfileScreen extends StatefulWidget {
   final User user;
@@ -20,12 +21,22 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
   File _profileImage;
   String _name = ' ';
   String _bio = ' ';
+  bool _isLoading = false;
 
-  _submit() {
+  _submit() async {
     if(_formKey.currentState.validate()) {
       _formKey.currentState.save();
+
+      setState(() {
+        _isLoading = true;
+      });
       // Update user in DB
       String _profileImageUrl = '';
+      if(_profileImageUrl == null) {
+        _profileImageUrl = widget.user.profileImageUrl;
+      } else {
+        _profileImageUrl = await StorageService.uploadUserProfileImage(widget.user.profileImageUrl, _profileImage);
+      }
       User user = User(id: widget.user.id, name: _name, bio: _bio, profileImageUrl: _profileImageUrl);
       // DB update
       DatabaseService.updateUser(user);
@@ -75,55 +86,61 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
         title: Text('Edit Profile', style: TextStyle(color: Colors.black),),
       ),
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          child: Padding(
-            padding: const EdgeInsets.all(30.0),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                children: <Widget>[
-                  CircleAvatar(
-                    radius: 60.0,
-                    backgroundColor: Colors.grey,
-                    backgroundImage: _displayPhotoImage(),
-                  ),
-                  FlatButton(
-                    onPressed: () => _handleImageFromGallery(),
-                    child: Text('Change Profile Image', style: TextStyle(color: Theme.of(context).accentColor, fontSize: 16.0),)
-                  ),
-                  TextFormField(
-                    initialValue: _name,
-                    style: TextStyle(fontSize: 18.0),
-                    decoration: InputDecoration(
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: ListView(
+          children: <Widget>[
+            _isLoading ? LinearProgressIndicator(
+              backgroundColor: Colors.blue[200],
+              valueColor: AlwaysStoppedAnimation(Colors.blue),
+            ) : SizedBox.shrink(),
+            Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    CircleAvatar(
+                      radius: 60.0,
+                      backgroundColor: Colors.grey,
+                      backgroundImage: _displayPhotoImage(),
+                    ),
+                    FlatButton(
+                        onPressed: () => _handleImageFromGallery(),
+                        child: Text('Change Profile Image', style: TextStyle(color: Theme.of(context).accentColor, fontSize: 16.0),)
+                    ),
+                    TextFormField(
+                      initialValue: _name,
+                      style: TextStyle(fontSize: 18.0),
+                      decoration: InputDecoration(
                         icon: Icon(Icons.person, size: 30.0,), labelText: 'Name',),
-                    validator: (input) => input.trim().length < 1 ? ' Please enter a valid name' : null,
-                    onSaved: (input) => _name = input,
-                  ),
-                  TextFormField(
-                    initialValue: _bio,
-                    style: TextStyle(fontSize: 18.0),
-                    decoration: InputDecoration(
-                      icon: Icon(Icons.book, size: 30.0,), labelText: 'Bio',),
-                    validator: (input) => input.trim().length > 150 ? ' Please enter a bio less than 150 characters' : null,
-                    onSaved: (input) => _bio = input,
-                  ),
-                  Container(
-                    margin: EdgeInsets.all(40.0),
-                    height: 40.0,
-                    width: 250.0,
-                    child: FlatButton(
-                      onPressed: _submit,
-                      color: Colors.blue,
-                      textColor: Colors.white,
-                      child: Text('Save Profile', style: TextStyle(fontSize: 18.0),),
+                      validator: (input) => input.trim().length < 1 ? ' Please enter a valid name' : null,
+                      onSaved: (input) => _name = input,
+                    ),
+                    TextFormField(
+                      initialValue: _bio,
+                      style: TextStyle(fontSize: 18.0),
+                      decoration: InputDecoration(
+                        icon: Icon(Icons.book, size: 30.0,), labelText: 'Bio',),
+                      validator: (input) => input.trim().length > 150 ? ' Please enter a bio less than 150 characters' : null,
+                      onSaved: (input) => _bio = input,
+                    ),
+                    Container(
+                        margin: EdgeInsets.all(40.0),
+                        height: 40.0,
+                        width: 250.0,
+                        child: FlatButton(
+                          onPressed: _submit,
+                          color: Colors.blue,
+                          textColor: Colors.white,
+                          child: Text('Save Profile', style: TextStyle(fontSize: 18.0),),
+                        )
                     )
-                  )
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
+          ],
         ),
       ),
     );
